7af69df844ed973702545f90c79bc22acc0c9086
Emirhan <emirhannkilicaslann@gmail.com>
2023-08-25 12:16:46 +0300

    Detail tags added

--- Models/Repository.cs
+++ Models/Repository.cs
@@ -13,10 +13,40 @@ namespace basics.Models
         static Repository(){
             _courses = new List<Course>()
             {
-                new Course() {Id = 1, Title = "Aspnet Kursu", Description = "Guzel bir kurs.", Image = "1.jpeg"},
-                new Course() {Id = 2, Title = "Python Kursu", Description = "Guzel bir kurs.", Image = "2.jpeg"},
-                new Course() {Id = 3, Title = "Angular Kursu", Description = "Guzel bir kurs.", Image = "3.jpeg"},
-                new Course() {Id = 4, Title = "Javascript Kursu", Description = "Guzel bir kurs.", Image = "1.jpeg"}                
+                new Course() {
+                    Id = 1,
+                    Title = "Aspnet Kursu",
+                    Description = "Guzel bir kurs.",
+                    Image = "1.jpeg",
+                    Tags = new string[] {"aspnet", "Web gelistirme"},
+                    isActive = true,
+                    isHome = true
+                },
+                new Course() {
+                    Id = 2,
+                    Title = "Python Kursu",
+                    Description = "Guzel bir kurs.",
+                    Image = "2.jpeg",
+                    Tags = new string[] {"python", "Web gelistirme"},
+                    isActive = true,
+                    isHome = true
+                },
+                new Course() {
+                    Id = 3,
+                    Title = "Angular Kursu",
+                    Description = "Guzel bir kurs.",
+                    Image = "3.jpeg",
+                    isActive = true,
+                    isHome = true
+                },
+                new Course() {
+                    Id = 4,
+                    Title = "Javascript Kursu",
+                    Description = "Guzel bir kurs.",
+                    Image = "1.jpeg",
+                    isActive = true,
+                    isHome = false
+                }                
             };
         }
 
